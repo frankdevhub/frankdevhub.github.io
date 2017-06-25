c03b2d716e6eb5fc1af2a4f18088e2d70df5eb08
frankdevhub <frankdevhub@163.com>
2017-06-25 23:50:48 +0800

    delete

--- category/android.md
+++
@@ -1,6 +0,0 @@
----
-layout: posts_by_category
-categories: android
-title: android
-permalink: /category/android
----
--- res/js/baidu.js
+++
@@ -1,9 +0,0 @@
-<script>
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?f2e6200c3d4a2e3db5276d77af6c7797";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-</script>
