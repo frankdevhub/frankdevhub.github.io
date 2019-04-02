d5c7d10195ee4ef370bf22b2df069c257d7d3c68
frankdevhub@163.com <frankdevhub@163.com>
2019-04-03 02:50:59 +0800

    generate sitemap.xml and robot.txt

--- sitemap.bat
+++ sitemap.bat
@@ -1,23 +1,25 @@
 
+SETLOCAL ENABLEDELAYEDEXPANSION
+
 ::--------------------------------------------------------
 ::-- @author frankdevhub@163.com 
 ::-- @Date 2019/04/03 Wednesday
 ::-- @description: generate sitemap and robot.txt
 ::--------------------------------------------------------
 
-set current_log_timestamp=""
+set current_log_timestamp=" "
 :getLocalTimeStamp
 
-set date_timestamp_format=%1
-set time_timestamp_format=%2
+	set date_timestamp_format="%1"
+	set time_timestamp_format="%2"
 
-if date_timestamp_format==1
-current_log_timestamp=%DATE:~4,4%%DATE:~9,2%%DATE:~12,2%
+	if date_timestamp_format==1
+	current_log_timestamp=%DATE:~4,4%%DATE:~9,2%%DATE:~12,2%
 
-if time_timestamp_format==1
-current_log_timestamp=%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%
+	if time_timestamp_format==1
+	current_log_timestamp=%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%
 
-goto:eof
+GOTO:EOF
 
 @echo 'start at call:getLocalTimeStamp 1 -1' 
 
@@ -25,13 +27,11 @@ goto:eof
 ::-- @description: install plugin jekyll-sitemap
 ::--------------------------------------------------------
 
-@echo off
-setlocal enabledelayedexpansion
 
 
-jekyll build &echo 'sss' 
+jekyll build &echo '---------jekyll build complete---------' 
 
 
+echo error
 
-
-& pause
+PAUSE
