8306f01f026d4526cd21f6d7be0bd93b8af69a17
frankdevhub@163.com <frankdevhub@163.com>
2019-04-07 03:00:54 +0800

    judge if remote resource exist

--- js/script-exist.js
+++ js/script-exist.js
@@ -1,22 +1,8 @@
  function isExistScript(url){
 	 console.log('url:'+url+'');
 	 //test url:https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js
-	 var xmlHttp ;  
-	 var timeOut = false;
-     setTimeout(sendXMLHttp(url),2500);	 
-	 	
-			   
-	if(xmlHttp.readyStatus==4){
-	if(xmlhttp.status==200)return false;
-	else if(xmlhttp.status==404)return true;
-	else return false;
-    }
-	return true;
- }
- 
- 
- function sendXMLHttp(url){
-	  if (window.ActiveXObject) 
+	 var xmlHttp;  
+	 if (window.ActiveXObject) 
          {  
           xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");  
          }  
@@ -25,11 +11,25 @@
           xmlHttp = new XMLHttpRequest();    
          }   
         xmlHttp.open("GET",url,false);  
+		//start timer
+		var timer = setTimeout(function(){
+               xhr.abort();
+		},2500);	 
         xmlHttp.send(null); 
-        console.log('xmlHttp start sending');		
-		//timer start   
+        console.log('xmlHttp start sending');	 
 	 
+		 xmlHttp.onreadystatechange = function(){
+				if(xmlHttp.readyStatus==4){
+					if(xmlhttp.status==200)return true;
+					else if(xmlhttp.status==404)return false;
+					else return false;
+				}
+				return true;
+		}	 
+			   
+
  }
+ 
  
  
  
