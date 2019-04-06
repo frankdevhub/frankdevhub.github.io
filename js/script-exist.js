03f0f8acbfcbe90befe9eda490cfc7ce379518e5
frankdevhub@163.com <frankdevhub@163.com>
2019-04-07 02:37:57 +0800

    try if can access remote source

--- js/script-exist.js
+++ js/script-exist.js
@@ -1,8 +1,22 @@
  function isExistScript(url){
 	 console.log('url:'+url+'');
+	 //test url:https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js
 	 var xmlHttp ;  
 	 var timeOut = false;
-        if (window.ActiveXObject) 
+     setTimeout(sendXMLHttp(url),2500);	 
+	 	
+			   
+	if(xmlHttp.readyStatus==4){
+	if(xmlhttp.status==200)return false;
+	else if(xmlhttp.status==404)return true;
+	else return false;
+    }
+	return true;
+ }
+ 
+ 
+ function sendXMLHttp(url){
+	  if (window.ActiveXObject) 
          {  
           xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");  
          }  
@@ -10,18 +24,10 @@
          {  
           xmlHttp = new XMLHttpRequest();    
          }   
-        xmlHttp.open("GET",url,true);  
-        xmlHttp.send();  
-		//timer start
-	    setTimeout(function(){
-			timeOut = true;
-			xmlHttp.abort();
-		},2500);
-        if(xmlHttp.readyStatus==4){
-            if(xmlhttp.status==200)return true;
-            else if(xmlhttp.status==404)return false;
-            else return false;
-        }  
+        xmlHttp.open("GET",url,false);  
+        xmlHttp.send(null); 
+        console.log('xmlHttp start sending');		
+		//timer start   
 	 
  }
  
