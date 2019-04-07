586b80d2cc050f81b00bca831c4950171cd736a6
frankdevhub@163.com <frankdevhub@163.com>
2019-04-08 00:33:54 +0800

    judge if remote resource exist

--- js/script-exist.js
+++ js/script-exist.js
@@ -1,8 +1,8 @@
  var xmlHttp;  
  
- function isExistScript(){
+ function isExistScript(url){
 	 //test only
-     url="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js";
+     //url="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js";
 	 console.log('url:' +url+ '');
 	 var timeout = false;
 	    if (window.ActiveXObject) 
