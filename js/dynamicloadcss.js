b5172ab5c5e513a9271e9107c95e618552190574
frankdevhub@163.com <frankdevhub@163.com>
2019-04-08 15:19:10 +0800

    debug dynamicload css in pages using google map api

---
+++ js/dynamicloadcss.js
@@ -0,0 +1,11 @@
+function dynamicLoadCss(url){
+	
+	var head = documents.getElementsByTagName('head')[0];
+	var link = document.createElement('link');
+	
+	link.type = 'type/css';
+	link.rel = 'stylesheet';
+	link.href = url;
+	
+	head.appendChild(link);
+}
