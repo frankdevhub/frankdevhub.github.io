42e3d0788362087cf88e4227372cec046752b41a
frankdevhub@163.com <frankdevhub@126.com>
2019-04-10 01:58:19 +0800

    if connection of google map javascript api failed then use local javascript api

--- js/google-map-api-net.js
+++ js/google-map-api-net.js
@@ -42,14 +42,22 @@ function drawGoogleMap(xmlHttp){
 			   console.log('show error dialog');
 			   var netErrorDialog = jqueryAlert({
 				'style'   : 'pc',
-				'title'   : '<span style="color:red">ERR_CONNECTION_REFUSED</span>',
-				'content' :  'Google Map API Service is Not Reachable!',
+				'title'   : '<span style="color:red">ERR_CONNECTION_REFUSED</span>'+
+				'content' : '<span>Google Map API Service is Not Reachable!</span>'+
+				            '<span>Using local Javascript API from </span>'+
+							'<span style="color:blue">https://www.frankdevhub.site</sapn>',
 				'modal'   : true,
 				'contentTextAlign' : 'center',
 				'width'   : 'auto',
 				'buttons' :{
 					'Close' : function(){
 						netErrorDialog.close();
+						//using local google map javascript api
+						
+						var url = "/js/maps-google-api.js";
+						dynamicloadjs(google_map_api,initMap);
+					
+						
 					}
 				}
 			})
