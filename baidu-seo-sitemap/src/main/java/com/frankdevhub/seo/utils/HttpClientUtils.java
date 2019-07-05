d1f186d461291b6a0f2b9b5c80594d73d62b004f
frankdevhub@163.com <frankdevhub@163.com>
2019-07-06 03:02:16 +0800

    update baidu seo utils

---
+++ baidu-seo-sitemap/src/main/java/com/frankdevhub/seo/utils/HttpClientUtils.java
@@ -0,0 +1,15 @@
+package com.frankdevhub.seo.utils;
+
+/**
+ * <p>Title:@ClassName HttpClientUtils.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2019</p>
+ * <p>Company: www.frankdevhub.site</p>
+ * <p>github: https://github.com/frankdevhub</p>
+ *
+ * @Author: frankdevhub@gmail.com
+ * @CreateDate: 2019/7/6 3:02
+ * @Version: 1.0
+ */
+public class HttpClientUtils {
+}
