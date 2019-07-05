5d3977078b3b073059e4ad2756e225d3d4f0f152
frankdevhub@163.com <frankdevhub@163.com>
2019-07-06 01:59:29 +0800

    update baidu seo utils

---
+++ baidu-seo-sitemap/src/main/java/com/frankdevhub/seo/BaiDuSeoApp.java
@@ -0,0 +1,18 @@
+package com.frankdevhub.seo;
+
+/**
+ * <p>Title:@ClassName BaiDuSeoApp.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2019</p>
+ * <p>Company: www.frankdevhub.site</p>
+ * <p>github: https://github.com/frankdevhub</p>
+ *
+ * @Author: frankdevhub@gmail.com
+ * @CreateDate: 2019/7/6 1:58
+ * @Version: 1.0
+ */
+public class BaiDuSeoApp {
+    public static void main(String[] args) {
+
+    }
+}
