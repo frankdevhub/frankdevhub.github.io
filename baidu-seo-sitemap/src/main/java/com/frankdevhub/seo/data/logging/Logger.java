7b9f50f1f5b8c6bff96d6f5fbaccebba217899da
frankdevhub@163.com <frankdevhub@163.com>
2019-07-06 02:46:47 +0800

    update baidu seo utils

--- baidu-seo-sitemap/src/main/java/com/frankdevhub/seo/data/logging/Logger.java
+++ baidu-seo-sitemap/src/main/java/com/frankdevhub/seo/data/logging/Logger.java
@@ -5,36 +5,37 @@ import com.frankdevhub.seo.message.logging.ELoggerImpl;
 
 
 /**
- * <p>Title:Logger.java</p>  
- * <p>Description: </p>  
- * <p>Copyright: Copyright (c) 2019</p>  
+ * <p>Title:Logger.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2019</p>
  * <p>Company: www.frankdevhub.site</p>
- * <p>github: https://github.com/frankdevhub</p>  
- * @author frankdevhub   
+ * <p>github: https://github.com/frankdevhub</p>
+ *
+ * @author frankdevhub
  * @date:2019-04-20 22:43
  */
 
 public class Logger {
-	private Class<?> class1;
-	private org.slf4j.Logger logger;
-	private ConsoleSender sender = new ConsoleSender();
+    private Class<?> class1;
+    private org.slf4j.Logger logger;
+    private ConsoleSender sender = new ConsoleSender();
 
-	public Logger(){
-		
-	}
-	
-	public Logger(Class<?> class1) {
-		this.class1 = class1;
-	}
+    public Logger() {
 
-	public Logger(org.slf4j.Logger logger) {
-		this.logger = logger;
-	}
+    }
 
-	public ELoggerImpl begin() {
-		if (this.logger == null) {
-			return new ELoggerImpl(this.sender, this.class1);
-		}
-		return new ELoggerImpl(this.sender, this.logger.getClass());
-	}
+    public Logger(Class<?> class1) {
+        this.class1 = class1;
+    }
+
+    public Logger(org.slf4j.Logger logger) {
+        this.logger = logger;
+    }
+
+    public ELoggerImpl begin() {
+        if (this.logger == null) {
+            return new ELoggerImpl(this.sender, this.class1);
+        }
+        return new ELoggerImpl(this.sender, this.logger.getClass());
+    }
 }
---
+++ baidu-seo-sitemap/src/main/java/com/frankdevhub/seo/utils/PostUtils.java
@@ -0,0 +1,28 @@
+package com.frankdevhub.seo.utils;
+
+import com.frankdevhub.seo.data.logging.Logger;
+import com.frankdevhub.seo.data.logging.LoggerFactory;
+
+import java.util.Properties;
+
+/**
+ * <p>Title:@ClassName PostUtils.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2019</p>
+ * <p>Company: www.frankdevhub.site</p>
+ * <p>github: https://github.com/frankdevhub</p>
+ *
+ * @Author: frankdevhub@gmail.com
+ * @CreateDate: 2019/7/6 2:42
+ * @Version: 1.0
+ */
+public class PostUtils {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PostUtils.class);
+
+    private String usrToken;
+    private Properties usrProperties;
+
+    private void readConfiguration() {
+
+    }
+}
