21bb93fd3d874540ce9f5b0973c87d01763de1bf
frankdevhub@163.com <frankdevhub@163.com>
2019-07-06 12:12:45 +0800

    update baidu seo utils

--- baidu-seo-sitemap/src/main/java/com/frankdevhub/seo/utils/PostUtils.java
+++ baidu-seo-sitemap/src/main/java/com/frankdevhub/seo/utils/PostUtils.java
@@ -9,6 +9,8 @@ import org.springframework.core.io.ClassPathResource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * <p>Title:@ClassName PostUtils.java</p>
@@ -25,6 +27,7 @@ public class PostUtils {
     private static final Logger LOGGER = LoggerFactory.getLogger(PostUtils.class);
 
     private String usrToken;
+    private String url;
     private Properties usrProperties;
 
     private void readConfiguration() throws Exception {
@@ -33,12 +36,35 @@ public class PostUtils {
         usrProperties = new Properties();
         usrProperties.load(inputStream);
 
+        //get token
         String token = usrProperties.getProperty("token");
         if (StringUtils.isBlank(token))
             throw new Exception("[Error]:token string should not be empty!");
         System.out.println(String.format("your token is:[%s]", token));
         this.usrToken = token;
+        //get url;
+        String url = usrProperties.getProperty("url");
+        if (StringUtils.isBlank(url))
+            throw new Exception("[Error]:url string should not be empty!");
+        //validate url format
+        if (!validateUrlString(url))
+            throw new Exception("[Error]:invalid url format,should be as \"www.example.com\"!");
+        System.out.println(String.format("your url is:[%s]", url));
+        this.url = url;
+
     }
+
+    private boolean validateUrlString(String url) {
+        boolean valid = false;
+        String urlRegx = "(^([wW]{3}.|[wW][aA][pP].|[fF][tT][pP].|[fF][iI][lL][eE].)([-A-Za-z0-9#_]+.)([-A-Za-z0-9#_]" +
+                "+)[/|&](([-A-Za-z0-9+&@#/%?=~_|!:;]+)?))$";
+        Pattern pattern = Pattern.compile(urlRegx);
+        Matcher matcher = pattern.matcher(url);
+        if (matcher.find())
+            return true;
+        return valid;
+    }
+
 
     public void doPostLinks() throws Exception {
         try {
