175d1f590792ba5a7a028d7705b01fc78ed5defe
FangChenSheng <frankdevhub@gmail.com>
2020-03-22 23:41:43 +0800

    parse siteMap document, push post linkd to seo platform

--- plugin/site-plugin/src/main/java/com/frankdevhub/site/core/utils/SiteMapParseUtils.java
+++ plugin/site-plugin/src/main/java/com/frankdevhub/site/core/utils/SiteMapParseUtils.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
@@ -14,6 +16,21 @@ import org.dom4j.io.SAXReader;
 import org.springframework.util.Assert;
 
 public class SiteMapParseUtils {
+
+	private String hostName = "localhost";
+	private String port = "4000";
+	private String domain = "www.frankdevhub.site";
+
+	private final static String REGEX_IP = "^((2(5[0-5]|[0-4]\\d))|[0-1]?\\d{1,2})(\\.((2(5[0-5]|[0-4]\\d))|[0-1]?\\d{1,2})){3}$";
+	private final static String REGEX_HTTP = "([hH][tT]{2}[pP]://|[hH][tT]{2}[pP][sS]://)";
+
+	public SiteMapParseUtils(String hostName, String port, String domain) {
+		if (null != hostName)
+			this.hostName = hostName;
+		if (null != port)
+			this.port = port;
+		this.domain = domain;
+	}
 
 	public Map<Object, Object> parseXMLDocument(File file) throws DocumentException {
 		Map<Object, Object> res = new HashMap<>();
@@ -41,22 +58,62 @@ public class SiteMapParseUtils {
 		return res;
 	}
 
+	private void validatePostHost() {
+		if (!this.hostName.equals("localhost")) {
+			boolean find = Pattern.matches(REGEX_IP, this.hostName);
+			Assert.isTrue(find, "invalid hostname:[" + this.hostName + "]");
+		}
+		if (null != this.port)
+			Integer.parseInt(this.port);
+	}
+
+	private boolean validateLinkUrl(String url) {
+		boolean valid = false;
+		String regex = new StringBuffer(REGEX_HTTP).append("(").append(this.hostName).append(":").append(this.port)
+				.append("/").append(")").toString();
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(url);
+		valid = m.find();
+		return valid;
+	}
+
 	public Map<Object, Object> pushToBaiduPlatform(List<String> urls) {
+		validatePostHost();
 		Map<Object, Object> res = new HashMap<>();
+
+		res.put("origin_count", urls.size());
+		int submitCount = 0;
+		int succeedCount = 0;
+		int failureCount = 0;
+		long start = System.currentTimeMillis();
+
+		for (String url : urls) {
+			if (validateLinkUrl(url))
+				submitCount++;
+			try {
+				pushToBaiduPlatform(url);
+				succeedCount++;
+			} catch (Exception e) {
+				e.printStackTrace();
+				failureCount++;
+			}
+		}
+
+		long end = System.currentTimeMillis();
+		double timecost = (end - start) / 1000;
+
+		res.put("submit_count", submitCount);
+		res.put("succeed_count", succeedCount);
+		res.put("failure_count", failureCount);
+		res.put("timecost", timecost);
 
 		return res;
 	}
 
 	private boolean pushToBaiduPlatform(String url) {
 		boolean succeed = false;
+
 		return succeed;
 	}
 
-	public static void main(String[] args) throws Exception {
-		File file = new File("D://sitemap.xml");
-		if (!file.exists())
-			throw new Exception("file not exists");
-		new SiteMapParseUtils().parseXMLDocument(file);
-
-	}
 }
